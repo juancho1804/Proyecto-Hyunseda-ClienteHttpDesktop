a317ad38e67a7f315cdd3ccd2dedc9d1bd607111
juancho1804 <juanmanuelceron@unicauca.edu.co>
2024-05-27 01:29:47 -0500

    Avanzando en javaFX

--- ClientProductHttpClient/src/main/java/com/unicauca/clientproducthttpclient/controllers/HomeAdminController.java
+++ ClientProductHttpClient/src/main/java/com/unicauca/clientproducthttpclient/controllers/HomeAdminController.java
@@ -3,9 +3,11 @@ package com.unicauca.clientproducthttpclient.controllers;
 import com.unicauca.clientproducthttpclient.util.Utilities;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -17,7 +19,19 @@ public class HomeAdminController{
     @FXML
     private Button btnCerrarSesion;
     @FXML
+    private Button btnInicio;
+    @FXML
+    private Button btnProductos;
+    @FXML
+    private Button btnCategorias;
+    @FXML
     private Label lblUsuario;
+    @FXML
+    private AnchorPane pnlInicio;
+    @FXML
+    private AnchorPane pnlProductos;
+
+
 
     public void setLblUsuario(String text){
         lblUsuario.setText(text);
@@ -25,7 +39,14 @@ public class HomeAdminController{
 
 
 
-
+    public void btnOnActionInicio(ActionEvent event){
+        pnlProductos.setVisible(false);
+        pnlInicio.setVisible(true);
+    }
+    public void btnOnActionProductos(ActionEvent event){
+        pnlInicio.setVisible(false);
+        pnlProductos.setVisible(true);
+    }
     public void btnOnActionCerrarSesion(ActionEvent event) {
         Stage stage = (Stage) btnCerrarSesion.getScene().getWindow();
         Utilities.cargarFXML("/views/login.fxml","Login");
